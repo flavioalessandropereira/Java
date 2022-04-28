4f38e8b2985766fd3fa89b3d6cb5064342797498
flavioalessandropereira <flavioalessandropereira@gmail.com>
2022-04-28 17:30:21 -0300

    adicionado o README

---
+++ 1-ConceituacaoCriacao/src/br/com/dio/main.java
@@ -0,0 +1,47 @@
+package br.com.dio;
+
+public class main {
+    public static void main(String[] args) {
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;  // valor constante de j = 10
+        //j = 15;
+        int asrn2467md;
+        //int asrn246 78md;
+        int asrn2$467md = 10;
+        //int asrn2$46%7_md;
+
+        asrn2467md = 100;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto; //não inicia variavel com letra Maiúscula
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn2467md);
+        System.out.println(asrn2$467md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+    }
+
+}
